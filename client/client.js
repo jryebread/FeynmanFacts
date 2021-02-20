73cc0d751ffdceb6161476d2a1e19151d7c4bae2
James Riback <ribackj@amazon.com>
2021-02-20 15:49:52 -0800

    added site search functionality

--- client/client.js
+++ client/client.js
@@ -1,9 +1,11 @@
-console.log("Hello world!")
-
 const form = document.querySelector('form');
 const loadingElement = document.querySelector('.loading');
+const mewsElement = document.querySelector('.mews');
+const API_URL = 'http://localhost:5000/mews'; //post request against this URL
 
-loadingElement.style.display = 'none';
+loadingElement.style.display = '';
+
+listAllMews();
 
 form.addEventListener('submit', (event) => {
     event.preventDefault();
@@ -16,8 +18,57 @@ form.addEventListener('submit', (event) => {
         content
     };
 
-    console.log(mew);
     form.style.display = 'none';
     loadingElement.style.display = '';
-    console.log("form was subbed!")
-})
+    
+    //SUBMIT FORM
+    fetch(API_URL, {
+        method:'POST',
+        body: JSON.stringify(mew),
+        headers: {
+            'content-type': 'application/json'
+        }
+    }).then(response => response.json())
+      .then(createdMew => {
+        
+        console.log(createdMew);
+        form.reset()
+        setTimeout(() => {
+            form.style.display="";
+        }, 30000);
+        form.style.display = '';
+        loadingElement.style.display='none';
+        listAllMews();
+      });
+});
+
+//get endpoint to do GET request to nodejs server
+function listAllMews() {
+    mewsElement.innerHTML = '';
+    fetch(API_URL)
+        .then(response => response.json())
+        .then(mews => {
+            mews.reverse();
+            mews.forEach(mew => {
+                const div = document.createElement('div');
+
+                const header = document.createElement('h3');
+                header.textContent = mew.name;
+                
+                const contents = document.createElement('p');
+                contents.textContent = mew.content;
+
+                const date = document.createElement('small');
+                console.log(mew.date);
+                date.textContent = new Date(mew.date);
+
+                div.appendChild(header);
+                div.appendChild(contents);
+                div.appendChild(date);
+
+                mewsElement.appendChild(div);
+                loadingElement.style.display = 'none';
+
+            })
+        })
+}
